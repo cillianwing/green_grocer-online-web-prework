c25d1e0533fe8c05a9c88347413dcbffac08be46
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-03-11 09:43:42 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,17 @@
 def consolidate_cart(cart)
   # code here
+  final_cart = {}
+  cart.each do |cart_hash|
+    cart_hash.each do |item, info|
+      if final_cart == nil || final_cart.has_key?(item) == false 
+        final_cart[item] = info 
+        final_cart[item][:count] = 1 
+      elsif final_cart.has_key?(item)
+        final_cart[:count] += 1 
+      end
+    end
+  end
+  final_cart
 end
 
 def apply_coupons(cart, coupons)
